5254427288a6b813cdd457cb08a1df91bcc903df
GianlucaTravasci <gianlucatravasci@gmail.com>
2020-08-27 17:26:10 +0200

    added all the backend functionality

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,14 +1,14 @@
 import React from 'react';
 //import './ImageLinkForm.css'
 
-const Rank = () => {
+const Rank = ({name, entries}) => {
     return (
         <div>
             <div className='white f3'>
-            {'Gianluca, your current rank is...'}
+                {`${name} , your current rank is...`}
             </div>
-            <div className='white f3'>
-                {'#5'}
+            <div className='white f1'>
+                {entries}
             </div>
         </div>
     );
