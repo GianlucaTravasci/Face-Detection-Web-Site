cc7aac0375e10efc52f995842a4a7dab27997416
GianlucaTravasci <gianluca.travasci@gmail.com>
2020-09-27 10:58:32 +0200

    add the logout functionality on dropdown

--- src/components/Profile/ProfileIcon.js
+++ src/components/Profile/ProfileIcon.js
@@ -3,21 +3,21 @@ import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap
 
 import './ProfileIcon.css';
 
-const ProfileIcon = (props) => {
+const ProfileIcon = ({ onRouteChange }) => {
 
     const [dropdownOpen, setDropdownOpen] = useState(false);
 
     const toggle = () => setDropdownOpen(prevState => !prevState);
 
     return (
-        <div class="pa4 tc">
+        <div className="pa4 tc">
             <Dropdown isOpen={dropdownOpen} toggle={toggle}>
                 <DropdownToggle tag="span" data-toggle="dropdown" aria-expanded={dropdownOpen}>
                     <img src="http://tachyons.io/img/logo.jpg" className="br-100 h3 w3 dib" alt="avatar" />
                 </DropdownToggle>
                 <DropdownMenu className="b--transparent shadow-5" style={{backgroundColor: 'rgba(255,255,255,0.7)'}}>
                     <DropdownItem>View Profile</DropdownItem>
-                    <DropdownItem>Signout</DropdownItem>
+                    <DropdownItem onClick={() => onRouteChange('signout')}>Signout</DropdownItem>
                 </DropdownMenu>
             </Dropdown>
         </div>
