c72832e593f524cbe96be6b6aa5895a3b3c3678a
GianlucaTravasci <gianluca.travasci@gmail.com>
2020-09-27 10:40:01 +0200

    add the dropdown

--- src/components/Profile/ProfileIcon.js
+++ src/components/Profile/ProfileIcon.js
@@ -1,23 +1,33 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+
 import './ProfileIcon.css';
 
-class ProfileIcon extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            dropdownOpen: false
-        }
-    }
+const ProfileIcon = (props) => {
 
-    render() {
-        return (
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+
+    const toggle = () => setDropdownOpen(prevState => !prevState);
+
+    return (
+        <div class="pa4 tc">
+            <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+                <DropdownToggle caret>
+                    Dropdown
+                </DropdownToggle>
+                <DropdownMenu>
+                    <DropdownItem>View Profile</DropdownItem>
+                    <DropdownItem>Signout</DropdownItem>
+                </DropdownMenu>
+            </Dropdown>
             <div class="pa4 tc">
-            <img
+                <img
                 src="http://tachyons.io/img/logo.jpg"
-                class="br-100 h3 w3 dib" alt="avatar"/>
+                class="br-100 h3 w3 dib" alt="avatar"  
+                />
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default ProfileIcon;
