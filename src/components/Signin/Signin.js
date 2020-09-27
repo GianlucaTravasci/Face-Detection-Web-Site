c99790ed54804986bff979faaf9888b174ee8daa
GianlucaTravasci <gianluca.travasci@gmail.com>
2020-09-27 11:32:24 +0200

    fix the style

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Signin.css'
 
 class Signin extends React.Component {
     constructor(props) {
@@ -39,18 +40,18 @@ class Signin extends React.Component {
     render() {
         const { onRouteChange } = this.props;
         return(
-            <article className="br3 ba bg-near-white mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+            <article className="br3 ba  mv4 w-100 w-50-m w-25-l mw6 shadow-5 center" style={{backgroundColor: 'rgba(255,255,255,0.3)'}}>
                 <main className="pa4 black-80">
                     <div className="measure">
                         <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
                             <legend className="f1 fw6 ph0 mh0">Sign In</legend>
                             <div className="mt3">
                                 <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                                <input onChange={this.onEmailChange} className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
+                                <input onChange={this.onEmailChange} className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 hover-black" type="email" name="email-address"  id="email-address" />
                             </div>
                             <div className="mv3">
                                 <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                                <input onChange={this.onPasswordChange} className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
+                                <input onChange={this.onPasswordChange} className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 hover-black" type="password" name="password"  id="password" />
                             </div>
                         </fieldset>
                         <div className="">
