ebd98fab6021f7ab8094b379e6ea964053264f8d
GianlucaTravasci <gianluca.travasci@gmail.com>
2020-09-27 10:50:44 +0200

    add the style

--- src/components/Profile/ProfileIcon.js
+++ src/components/Profile/ProfileIcon.js
@@ -12,20 +12,14 @@ const ProfileIcon = (props) => {
     return (
         <div class="pa4 tc">
             <Dropdown isOpen={dropdownOpen} toggle={toggle}>
-                <DropdownToggle caret>
-                    Dropdown
+                <DropdownToggle tag="span" data-toggle="dropdown" aria-expanded={dropdownOpen}>
+                    <img src="http://tachyons.io/img/logo.jpg" className="br-100 h3 w3 dib" alt="avatar" />
                 </DropdownToggle>
-                <DropdownMenu>
+                <DropdownMenu className="b--transparent shadow-5" style={{backgroundColor: 'rgba(255,255,255,0.7)'}}>
                     <DropdownItem>View Profile</DropdownItem>
                     <DropdownItem>Signout</DropdownItem>
                 </DropdownMenu>
             </Dropdown>
-            <div class="pa4 tc">
-                <img
-                src="http://tachyons.io/img/logo.jpg"
-                class="br-100 h3 w3 dib" alt="avatar"  
-                />
-            </div>
         </div>
     );
 }
